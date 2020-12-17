0142f74118a18717ab39980d9ee417d33741ee19
Michaell Oliveira <michaelloliveira@gmail.com>
2020-12-16 22:22:01 -0300

    resend

--- src/App.js
+++ src/App.js
@@ -1,55 +1,70 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import {
   SafeAreaView,
   View,
   FlatList,
   Text,
-  StatusBar,
   StyleSheet,
   TouchableOpacity,
 } from "react-native";
 
+import api from './services/api';
+
 export default function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      setRepositories(response.data);
+    });
+  }, []);
+
   async function handleLikeRepository(id) {
-    // Implement "Like Repository" functionality
+    const response = await api.post(`repositories/${id}/like`);
+
+    const updateRepositories = repositories.map((repository) =>
+      repository.id !== id ? repository : response.data
+    );
+
+    setRepositories(updateRepositories);
   }
 
   return (
     <>
-      <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
       <SafeAreaView style={styles.container}>
-        <View style={styles.repositoryContainer}>
-          <Text style={styles.repository}>Repository 1</Text>
+        <FlatList
+          data={repositories}
+          keyExtractor={repository => repository.id}
+          renderItem={({ item: repository }) => (
+            <View style={styles.repositoryContainer}>
+              <Text style={styles.repository}>{repository.title}</Text>
 
-          <View style={styles.techsContainer}>
-            <Text style={styles.tech}>
-              ReactJS
-            </Text>
-            <Text style={styles.tech}>
-              Node.js
-            </Text>
-          </View>
+              <View style={styles.techsContainer}>
 
-          <View style={styles.likesContainer}>
-            <Text
-              style={styles.likeText}
-              // Remember to replace "1" below with repository ID: {`repository-likes-${repository.id}`}
-              testID={`repository-likes-1`}
-            >
-              3 curtidas
-            </Text>
-          </View>
+                {repository.techs.map(tech => (
+                  <Text key={tech} style={styles.tech}>
+                    {tech.name}
+                  </Text>
+                )
+                )}
+              </View>
 
-          <TouchableOpacity
-            style={styles.button}
-            onPress={() => handleLikeRepository(1)}
-            // Remember to replace "1" below with repository ID: {`like-button-${repository.id}`}
-            testID={`like-button-1`}
-          >
-            <Text style={styles.buttonText}>Curtir</Text>
-          </TouchableOpacity>
-        </View>
+              <View style={styles.likesContainer}>
+                <Text
+                  style={styles.likeText}
+                  testID={`repository-likes-${repository.id}`}> {repository.likes} curtidas </Text>
+
+                <TouchableOpacity
+                  style={styles.button}
+                  onPress={() => handleLikeRepository(repository.id)}
+                  testID={`like-button-${repository.id}`}>
+                  <Text style={styles.buttonText}>Curtir</Text>
+                </TouchableOpacity>
+              </View>
+            </View>
+          )}
+        />
       </SafeAreaView>
     </>
   );
@@ -60,11 +75,33 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#7159c1",
   },
+  row: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  head: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  input: {
+    backgroundColor: "#fff",
+    borderRadius: 4,
+    margin: 15,
+    paddingLeft: 20,
+    flex: 1,
+  },
+  buttonAdd: {
+
+  },
   repositoryContainer: {
     marginBottom: 15,
     marginHorizontal: 15,
     backgroundColor: "#fff",
     padding: 20,
+    borderRadius: 4,
+    elevation: 10,
   },
   repository: {
     fontSize: 32,
@@ -87,14 +124,21 @@ const styles = StyleSheet.create({
     marginTop: 15,
     flexDirection: "row",
     justifyContent: "space-between",
+    alignItems: "center",
   },
   likeText: {
     fontSize: 14,
     fontWeight: "bold",
+    backgroundColor: "#5C42B3",
+    padding: 15,
+    color: "#fff",
     marginRight: 10,
+    marginTop: 10,
   },
   button: {
     marginTop: 10,
+    borderRadius: 4,
+    flex: 1,
   },
   buttonText: {
     fontSize: 14,
